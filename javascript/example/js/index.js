e013c6ea69f353a58671c44b4a4f784dc42fd94a
bymcs <56798318+bymcs@users.noreply.github.com>
2023-04-27 21:49:45 +0300

    Update index.js

--- javascript/example/js/index.js
+++ javascript/example/js/index.js
@@ -2,13 +2,30 @@ let counter = 0
 let counterDOM = document.querySelector('#counter')
 let arttirDOM = document.querySelector('#arttir')
 let azaltDOM = document.querySelector('#azalt')
+let testDOM = document.querySelector('#test')
 
-counterDOM.innerHTML = counter
+if(!localStorage.getItem('counter')){
+    localStorage.setItem('counter',counter)
+}
+
+counterDOM.innerHTML = localStorage.getItem('counter')
+
 
 arttirDOM.addEventListener("click", clickEvent)
 azaltDOM.addEventListener("click", clickEvent)
+counterDOM.addEventListener("click",clickEventTwo)
+testDOM.addEventListener("click",testFunc)
 
 function clickEvent(){
+    
     this.id === "arttir" ? counter += 1 : counter -= 1
     counterDOM.innerHTML = counter
+    localStorage.setItem('counter',counter)
 }
+
+function clickEventTwo(){
+    
+    this.style.color == 'red' ? this.style.color = 'yellow' : this.style.color = 'red' 
+}
+
+function testFunc(){ alert('Merhaba')}
